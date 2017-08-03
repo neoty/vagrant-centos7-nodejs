a1fd6bb2b900e353dcc2e8baec4caf47d99c8f1d
neoty <neoty@naver.com>
2017-08-03 22:02:10 +0900

    이것저것 수정 사항

--- Vagrantfile
+++ Vagrantfile
@@ -2,12 +2,29 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
+    
+    private_ip = "192.168.33.10"
+    hostname = "centos-nodejs"
+    cpus = 2
+    memory = 1024
+    host_source_synced_folder = "/Volumes/data/004. source/001. neoty"
+    guest_source_synced_folder = "/home/source"
+
     config.vm.box = "centos/7"
-    config.vm.define "centosNodejs" do |centosNodejs|
-        config.vm.network "private_network", ip: "192.168.33.10"
+    config.vm.define "vagrant-centos7-nodejs" do |vagrant_centos7_nodejs|
+        config.vm.network "private_network", ip: private_ip
         config.vm.hostname = "centos-nodejs"
     end
+
+    config.vm.provider "virtualbox" do |v|
+        v.memory = memory
+        v.cpus = cpus
+    end
+
     config.vm.synced_folder ".", "/home/vagrant/share", :mount_options => ["dmode=777", "fmode=666"]
-    config.vm.synced_folder "/Volumes/data/004. source/001. neoty", "/home/source", :mount_options => ["dmode=777", "fmode=666"]
+    config.vm.synced_folder host_source_synced_folder, guest_source_synced_folder, :mount_options => ["dmode=777", "fmode=666"]
+
+    # 최초 vagrant up 할시 setup.sh 내용을 읽어들여 실행시킵니다.
+    # setup.sh 를 수정할시 vagrant provision 해주세요.
     config.vm.provision :shell, privileged: false, path: 'setup.sh'
 end
--- setup.sh
+++ setup.sh
@@ -1,28 +1,62 @@
 echo "Provision virtual machine..."
+
 echo "Updating Packages"
-  sudo yum update -y > /dev/null 2>&1
+ sudo yum update -y > /dev/null 2>&1
+
 echo "nvm install"
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
 echo "source bashrc"
-  source .bashrc
+ source .bashrc
+
 echo "nodejs lts install"
-  nvm install lts/*
+ nvm install lts/*
+
 echo "nginx repo install"
-  sudo cp /home/vagrant/share/resources/nginx.repo /etc/yum.repos.d/nginx.repo
+ sudo cp /home/vagrant/share/resources/nginx.repo /etc/yum.repos.d/nginx.repo
+
 echo "nginx install"
  sudo yum install nginx -y
+
 echo "nginx regist systemctl"
  sudo systemctl enable nginx
+
 echo "pm2 install"
  npm install pm2 -g
+
 echo "mariadb 5 install"
  sudo yum install mariadb-server -y
+ sudo systemctl enable mariadb
+
+echo "mysql copy my.cnf"
+ sudo cp -r /home/vagrant/share/resources/my.cnf /etc/my.cnf 
+
+echo "mariadb restart"
+ sudo service mariadb restart
+
+echo "mariadb mysql_secure_installation"
+mysql -u root <<-EOF
+UPDATE mysql.user SET Password=PASSWORD('vagrant') WHERE User='root';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+FLUSH PRIVILEGES;
+EOF
+
 echo "wget install"
  sudo yum install wget -y
+
 echo "yarn install"
  npm install yarn -g
+
 echo "epel install"
- sudo rpm -ivh /home/vagrant/share/resources/epel-release-7-10.noarch.rpm
+ sudo rpm -Uvh /home/vagrant/share/resources/epel-release-7-10.noarch.rpm
+
 echo "htop"
  sudo yum install htop -y
+
+echo "selinux off"
+ sudo sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+ sudo setenforce 0
+
 echo "vagrant ssh 접속후 sudo mysql_secure_installation 해주세요!"
